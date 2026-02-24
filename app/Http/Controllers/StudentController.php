4f8bbba8f1e750a3a37a5d8c984aa8ee518af30f
Nambi Nandhini <nambinandhini001@gmail.com>
2026-02-24 23:21:46 +0530

    Initial commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+{
+    return Student::all();
+}
+
+public function store(Request $request)
+{
+    return Student::create($request->all());
+}
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
